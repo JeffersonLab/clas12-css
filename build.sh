2769d6528e204f0373d9a91fcd962b4eeae0592f
Wesley Moore <wmoore@jlab.org>
2015-09-24 23:24:34 -0400

    jlab build setup and script

---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+TOP=`pwd`
+MVN_CMD="mvn -X --settings $TOP/settings.xml clean verify"
+
+# order matters!!!
+MODULES="maven-osgi-bundles \
+    cs-studio-thirdparty \
+    cs-studio/core \
+    cs-studio/applications \
+    org.csstudio.sns"
+
+start=$(date +"%s")
+cols=`tput cols`
+FAILED=0
+
+function printbar {
+    printf "=%.0s" $(seq 1 $cols)
+}
+
+for mod in $MODULES
+do
+	cd $TOP/$mod
+    $MVN_CMD
+    if [ $? -eq 0 ]; then
+        printbar
+        printf "\n%s %s\n" $mod passed
+        printbar
+    else
+        FAILED=1
+        printbar
+        printf "\n%s %s\n" $mod failed
+        printbar
+        break
+    fi
+done
+
+#cd org.cstudio.clas12
+#mvn clean verify
+#cd $TOP
+
+end=$(date +"%s")
+tdiff=$(($end-$start))
+
+printbar
+if [ $FAILED -eq 0 ]; then
+    printf "\nBuild successful\n\n"
+else
+    printf "\nBuild failed\n\n"
+fi
+printf "Time Elapsed:  %dm%ds\n" $(($tdiff/60)) $(($tdiff%60))
+printbar
+
+exit
+
