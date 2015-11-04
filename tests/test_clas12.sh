064cb3d0ad4b66c39d0c54c7b231eeb080992a3b
Wesley Moore <wmoore@jlab.org>
2015-11-04 15:00:12 -0500

    test_*: added unzip force overwrite

--- tests/test_clas12.sh
+++ tests/test_clas12.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-unzip ../downloads/sns-css-4.1.1-linux.gtk.x86_64.zip
+unzip -o ../downloads/sns-css-4.1.1-linux.gtk.x86_64.zip
 cd sns-css-4.1.1
 ./css \
     -share_link /usr/clas12/release/dev/epics/css_share=/CLAS12_Share \
