9e014a8266ef1222c7358cedf3e0941a56675267
Wesley Moore <wmoore@jlab.org>
2015-11-04 14:54:51 -0500

    added tests/ for testing offsite

---
+++ tests/test_glascow.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+unzip ../downloads/sns-css-4.1.1-linux.gtk.x86_64.zip
+cd sns-css-4.1.1
+./css \
+    -share_link /usr/clas12/release/dev/epics/css_share=/CLAS12_Share \
+    -pluginCustomization ../plugin_customization_glascow.ini \
+    -nosplash &
+
